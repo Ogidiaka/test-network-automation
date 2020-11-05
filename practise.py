e60fb4e5a6d053c6e7314914f9ee7f4d7805879c
Emoghene Ogidiaka <ogidiaka.emoghene@yahoo.com>
2020-11-04 12:33:22 -1200

    Delete practise.py

--- practise.py
+++
@@ -1,6 +0,0 @@
-#Here’s an example of a simple script to log in to a router (an example IP is 192.168.255.249 with a username and password of cisco)
-from netmiko import ConnectHandler
-device = ConnectHandler(device_type=’cisco_ios’, ip=’192.168.255.249′, username=’cisco’, password=’cisco’)
-output = device.send_command(“show version”)
-print (output)
-device.disconnect()
